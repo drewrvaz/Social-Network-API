665fdd3d9f8c4cac2fccf517f8a7cfbe788f9958
drewrvaz <drewrvaz@gmail.com>
2022-11-16 12:30:05 -0500

    Added reactionSchema subdocument model to Thought.js

--- models/Thought.js
+++ models/Thought.js
@@ -1,5 +1,34 @@
 const moment = require('moment/moment');
-const { Schema, model } = require('mongoose');
+const { Schema, model, Types } = require('mongoose');
+
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: ()=> new Types.ObjectId()
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+    }
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+)
 
 const thoughtSchema = new Schema(
   {
@@ -18,7 +47,7 @@ const thoughtSchema = new Schema(
       type: String,
       required: true
     },
-    recations: [reactionsSchema]
+    reactions: [reactionSchema]
   },
   {
     toJSON: {
